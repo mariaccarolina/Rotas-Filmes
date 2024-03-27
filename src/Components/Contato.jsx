cffe272006469a36c80847e3d8f39ac95286e23d
Maria Carolina <demycar24@gmail.com>
2024-03-27 10:52:13 -0300

    first commit

---
+++ src/Components/Contato.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Contato() {
+    return (
+        <section>
+            <h1>Contatos</h1>
+        </section>
+    )
+}
+export default Contato
---
+++ src/Components/Naoencontrado.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Naoencontrado() {
+    return(
+        <section>
+            <img src="https://as1.ftcdn.net/v2/jpg/02/99/61/74/1000_F_299617487_fPJ8v9Onthhzwnp4ftILrtSGKs1JCrbh.jpg" alt="" />
+        </section>
+    )
+}
+export default Naoencontrado
